88c8402089af23884ff01d81a70925e139de1997
AndriykoSTU <balfagor@bigmi.net>
2015-12-14 19:02:37 +0200

    all work

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -33,6 +33,7 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 end
 
+
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
